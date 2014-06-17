63a197bd46939c53df3f0344b5b4e9f5b5def030
Jesse Chan <jchan985@gmail.com>
2014-06-17 15:04:40 -0500

    addign helmholtz stuff

--- Scratch/cg_helmholtz.m
+++ Scratch/cg_helmholtz.m
@@ -3,15 +3,13 @@
 % BCs: left Dirichlet, right (nonconstant) inhomogeneous Neumann BC, and
 % penalty enforcement of nonzero BCs on top and bottom 
 
-function mixedDPG_poisson
+function cg_helmholtz
 
 Globals2D
 
 % Polynomial order used for approximation
-Ntrial = 2;
-Ntest = Ntrial+2;
+N = 2;
 
-N = Ntest;
 global k
 k = 1;
 
@@ -21,36 +19,29 @@ k = 1;
 % [Nv, VX, VY, K, EToV] = MeshReaderGambit2D('block2.neu');
 % [Nv, VX, VY, K, EToV] = MeshReaderGambit2D('Maxwell1.neu');
 % [Nv, VX, VY, K, EToV] = MeshReaderGambit2D('Maxwell05.neu');
-% [Nv, VX, VY, K, EToV] = MeshReaderGambit2D('Maxwell025.neu');
-[Nv, VX, VY, K, EToV] = MeshReaderGambit2D('Maxwell0125.neu');
-
+% [Nv, VX, VY, K, EToV] = MeshReaderGambit2D('Maxwell025.neu'); % 8 elements
+[Nv, VX, VY, K, EToV] = MeshReaderGambit2D('Maxwell0125.neu'); % 16 elements 
 % Initialize solver and construct grid and metric
 StartUp2D;
 
-NpTrial = (Ntrial+1);
-NpTest = (Ntest+1);
-PPB = numel(vmapB)*(NpTrial/NpTest)/4 % 4 boundaries - points per boundary of trial
+
+PPB = numel(vmapB)/4 % 4 boundaries - points per boundary
 PPW = 8;
 k = PPB/PPW
-
 % keyboard
 % PPW = PPB/k
-
 
 % get block operators
 [M, Dx, Dy] = getBlockOps();
 [AK, BK] = getVolOp(M,Dx,Dy);
 
-% forcing = 0
-f = zeros(Np*K,1);
-
 % make CG operators
 [R vmapBT] = getCGRestriction();
-[Rp Irp vmapBTr xr yr] = pRestrictCG(Ntrial); % restrict test to trial space 
-Rr = Rp*Irp';
-B = R*BK*Rr';
-RV = R*AK*R';
+
+% forcing = 0
+f = zeros(Np*K,1);
 b = R*M*f;
+B = R*BK*R';
 
 % BC data for u
 u0 = zeros(size(B,2),1);
@@ -71,9 +62,9 @@ dudn0 = nx(mapB).*dudx0(vmapB) + ny(mapB).*dudy0(vmapB);
 % penalty/robin BCs 
 % robin = y(vmapB) > 1 - NODETOL | x(vmapB) > 1-NODETOL; % top/right boundaries
 robin = abs(nx(mapB)+1) < NODETOL | abs(ny(mapB)+1) < NODETOL;
-% robin = ones(size(vmapB));
+% robin = zeros(size(vmapB));
 [Mb Eb] = getBoundaryMatrix(robin);
-B = B - 1i*k*R*Eb'*Mb*Eb*Rr';
+B = B - 1i*k*R*Eb'*Mb*Eb*R';
 b = b + R*Eb'*Mb*(-1i*k*u0(vmapB) + dudn0);
 
 % nonhomogeneous neumann BCs
@@ -81,51 +72,15 @@ b = b + R*Eb'*Mb*(-1i*k*u0(vmapB) + dudn0);
 % onlyBot = (y(vmapB) < -1 + NODETOL) & (x(vmapB) < 1-NODETOL);
 % neum = onlyLeft | onlyBot; % left/bottom boundary
 neum = abs(nx(mapB)-1) < NODETOL | abs(ny(mapB)-1) < NODETOL;
-% neum = zeros(size(vmapB));
+% neum = ones(size(vmapB))*0;
 [Mb Eb] = getBoundaryMatrix(neum(:)); 
 b = b + R*Eb'*Mb*dudn0;
 
-% add boundary trace?
-[Mb Eb] = getBoundaryMatrix(robin);
-% RV = RV - k*R*Eb'*Mb*Eb*R';
-
-RV = B*B' + 1e-4*R*M*R';
-% RV = RV - k*R*Eb'*Mb*Eb*R';
-% DuDnB = (spdiag(nx(mapB(:)))*Eb*Dx + spdiag(ny(mapB(:)))*Eb*Dy)*R';
-% RV = RV + k^2*DuDnB'*Mb*DuDnB;
-
-% make saddle point system
-A = [RV B;B' zeros(size(B,2))];
-b = [b; zeros(size(B,2),1)]; 
-
-% vmapBT(robin) = [];
-% b(vmapBT) = 0;
-% A(vmapBT,:) = 0; A(:,vmapBT) = 0;
-% A(vmapBT,vmapBT) = speye(length(vmapBT));
-
-% try Dir on bot/left
-% u0 = zeros(size(B,2),1);
-% onlyLeft = (xr < -1 + NODETOL) & (yr < 1-NODETOL);
-% onlyBot = (yr < -1 + NODETOL) & (xr < 1-NODETOL);
-% neum = onlyLeft | onlyBot; % left/bottom boundary
-% u0(vmapBTr) = neum.*u0f(xr,yr); 
-% U0 = [zeros(size(B,1),1);u0];
-% b = b - A*U0;
-% vmapBTr(~neum) = [];
-% vmapBTU = vmapBTr + size(B,1);
-% b(vmapBTU) = U0(vmapBTU);
-% A(vmapBTU,:) = 0; A(:,vmapBTU) = 0;
-% A(vmapBTU,vmapBTU) = speye(length(vmapBTU));
-
 % solve and prolong solution u to local storage
-U = (A\b);
-u = Rr'*U(size(B,1)+1:end);
+U = (B\b);
+u = R'*U;
 
-e = U(1:size(B,1));
-% err = e'*RV*e;
-e = R'*e;
-
-Nplot = 25; [xu,yu] = EquiNodes2D(Nplot); 
+% Nplot = 25; [xu,yu] = EquiNodes2D(Nplot); 
 % Nplot = Ntrial; [xu,yu] = Nodes2D(Nplot); 
 Nplot = 25; [xu,yu] = Nodes2D(Nplot); 
 [ru, su] = xytors(xu,yu);
@@ -138,15 +93,10 @@ Iu0 = u0f(xu(:),yu(:));
 
 figure 
 Iuh = Iu*reshape(u,Np,K);Iuh = Iuh(:);
-Ie = Iu*reshape(e,Np,K);Ie = Ie(:);
-% title('Mixed form of DPG')
 ax = [-1 1 -1 1 -1 1];
-subplot(2,1,1);color_line3(xu,yu,Iuh,Iuh,'.');axis(ax);view(2);%view(0,0)
-subplot(2,1,2);color_line3(xu,yu,Iu0,Iu0,'.');axis(ax);view(2);%view(0,0)
-figure
-subplot(2,1,1);color_line3(xu,yu,Ie,Ie,'.');%axis([-1 1 -1 1 -.1 .1])
-colorbar;view(2);%view(0,0);
-subplot(2,1,2);color_line3(xu,yu,Iuh-Iu0,Iuh-Iu0,'.');%axis([-1 1 -1 1 -.1 .1])
+subplot(3,1,1);color_line3(xu,yu,Iuh,Iuh,'.');axis(ax);view(2);%view(0,0)
+subplot(3,1,2);color_line3(xu,yu,Iu0,Iu0,'.');axis(ax);view(2);%view(0,0)
+subplot(3,1,3);color_line3(xu,yu,Iuh-Iu0,Iuh-Iu0,'.');%axis([-1 1 -1 1 -.1 .1])
 colorbar;view(2);%view(0,0);
 
 Vu = Vandermonde2D(Nplot,ru,su);
@@ -155,7 +105,7 @@ Nplotp = (Nplot+1)*(Nplot+2)/2;
 e = reshape(Iuh-Iu0,Nplotp,K);
 tmp = Mu*e;
 L2err = sqrt(abs(e(:)'*tmp(:)));
-subplot(2,1,1);title(['L2 error = ' num2str(L2err) ' at k = ' num2str(k) ' and ' num2str(PPW) ' ppw.'])
+title(['L2 error = ' num2str(L2err) ' at k = ' num2str(k) ' and ' num2str(PPW) ' ppw.'])
 
 % keyboard
 
@@ -171,10 +121,8 @@ Ks = Dx'*M*Dx + Dy'*M*Dy;
 global k;
 Trial = -k^2*M + Ks;
 % Test = k^2*M + Ks;
-% Test = (1/k^2)*M + Ks;
-% L = -k^2*speye(size(M)) - Dx*Dx - Dy*Dy;
-% Test = Ks + k^2*L'*M*L + 1e-6*M;
-Test = (k^2)*M + Ks;
+Test = k^2*M + Ks;
+
 % blkiM = kron(speye(K),inv(MassMatrix));
 % invM = spdiag(1./J(:))*blkiM; % J = h^2
 % Test = Trial*blkiM*Trial' + 1e-6*M;%(1/k^2)*M;
