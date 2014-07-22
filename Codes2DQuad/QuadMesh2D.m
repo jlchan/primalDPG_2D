1a5ba3d92905956608f2034904321d740fc0fc5a
Jesse Chan <jchan985@gmail.com>
2014-07-22 16:08:42 -0500

    updating with new mesh code

---
+++ Codes2DQuad/QuadMesh2D.m
@@ -0,0 +1,26 @@
+function [Nv, VX, VY, K, EToV] = QuadMesh2D(NK)
+
+Nx = NK;Ny = NK;
+
+Nxp = Nx+1;
+Nyp = Ny+1;
+Nv = Nxp*Nyp;
+K = Nx*Ny;
+
+x1D = linspace(-1,1,Nxp);
+[x y] = meshgrid(x1D);
+[I J] = meshgrid(1:Nxp);
+inds = (I-1)*Ny + (J+I-1);
+EToV = zeros(K,4);
+k = 1;
+for i = 1:Nx
+    for j = 1:Ny
+        EToV(k,:) = [inds(i,j) inds(i+1,j) inds(i+1,j+1) inds(i,j+1)  ];
+        k = k+1;
+    end
+end
+
+VX = x(:)';
+VY = y(:)';
+
+
