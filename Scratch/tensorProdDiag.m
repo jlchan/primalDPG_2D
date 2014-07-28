2ba48b747db4bc41f049944a37abf0c40b2353ce
Jesse Chan <jchan985@gmail.com>
2014-07-28 11:05:25 -0500

    updating tensor prod

--- Scratch/tensorProdDiag.m
+++ Scratch/tensorProdDiag.m
@@ -25,13 +25,14 @@ M1D = inv(V1D*V1D');
 D1D = Dmatrix1D(N,r1D,V1D);
 
 K1D = D1D'*M1D*D1D;
-% 1D BCs
+% 1D BCs8
 K1D(1,:) = 0;K1D(:,1) = 0;K1D(1,1) = 1; 
 K1D(Np1D,:) = 0;K1D(:,Np1D) = 0;K1D(Np1D,Np1D) = 1;
 M1D(1,:) = 0;M1D(:,1) = 0;M1D(1,1) = 1;
 M1D(Np1D,:) = 0;M1D(:,Np1D) = 0;M1D(Np1D,Np1D) = 1;
 
-Kk = kron(M1D,K1D) + kron(K1D,M1D);
+a = 100;
+Kk = a*kron(M1D,M1D) + kron(M1D,K1D) + kron(K1D,M1D);
 uT=Kk\b;
 
 %% diagonalization sol
@@ -41,16 +42,16 @@ uT=Kk\b;
 [Q, L] = eig(K1D,M1D); % they're eigenvectors for both inv(M)*K and K*inv(M)
 Q = Q*diag(1./sqrt(diag(Q'*M1D*Q))); %normalize in M
 [Lam LamT] = meshgrid(diag(L));
-iLam = 1./(Lam+LamT);
+iLam = 1./(a + Lam + LamT);
 
 % % equiv to these 1D dense ops
-% kronL = kron(L,eye(Np1D)) + kron(eye(Np1D),L);
+kronL = kron(.5*a*eye(Np1D) + L,eye(Np1D)) + kron(eye(Np1D),.5*a*eye(Np1D)+L);
 % u = kron(Q,Q)*( kronL\(kron(Q',Q')*b(:)));
 
 b = reshape(b,Np1D,Np1D);
 u = Q*(iLam.*(Q'*b*Q))*Q';
 uD = u(:);
 
-% plotSol(uD,75);
+plotSol(uD,75);
 plotSol(uT-uD,75)
 title(['norm of diff = ', num2str(norm(uT-uD))])
