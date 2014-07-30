a1323af7e70e44110481bd5b3dc25baddbd5778e
Jesse Chan <jchan985@gmail.com>
2014-07-30 10:44:28 -0500

    updating testnorm experiments

--- Scratch/mortarTestNorm.m
+++ Scratch/mortarTestNorm.m
@@ -3,7 +3,7 @@ function mortarTestNorm
 Globals2D;
 FaceGlobals2D
 
-N = 4; % when N = even, Nf = N-1, fails?
+N = 2; % when N = even, Nf = N-1, fails?
 Nf = N-2; % = N trial
 Nt = N-1; % = 
 %     Read in Mesh
@@ -15,7 +15,7 @@ Nt = N-1; % =
 % [Nv, VX, VY, K, EToV] = MeshReaderGambit2D('Maxwell025.neu'); 
 % [Nv, VX, VY, K, EToV] = MeshReaderGambit2D('Maxwell1.neu');
 
-[Nv, VX, VY, K, EToV] = QuadMesh2D(8);
+[Nv, VX, VY, K, EToV] = QuadMesh2D(32);
 
 
 % Initialize solver and construct grid and metric
@@ -34,21 +34,21 @@ O = sparse(size(M,1),size(M,2));
 %          Div O];
 
 % % convection-diffusion
-% ep = .1;
-% Adj_h = [(1/ep)*I2 Grad;
-%          Div -Dx];
+ep = .0001;
+Adj_h = [(1/ep)*I2 Grad;
+         Div -Dx];
 
 % Helmholtz
-px = sqrt(K)*(N+1);
-k = px/4;
-Adj_h = [1i*k*I2 Grad;
-         Div 1i*k*I];
+% px = sqrt(K)*(N+1);
+% k = sqrt(K);
+% Adj_h = [1i*k*I2 Grad;
+%          Div 1i*k*I];
 
 M3 = blkdiag(M2,M);
 RV = Adj_h'*M3*Adj_h + 0*blkdiag(M,M,M); % regularize on v
 
 f = x(:).^0;
-btau = M2*[f;f];
+btau = M2*[f;f]*0;
 bv = M*f;
 % bv = bv*0;
 % bv(end-10) = 1;
@@ -68,7 +68,7 @@ Bf(fmapB(abs(1+xfb)<NODETOL & abs(nxfb+1)<NODETOL),:) = []; %remove constraints 
 [mv nv] = size(Bf);
 B = [Bt sparse(mtau,nv);
      sparse(mv,ntau) Bf];
- 
+% B = B*Adj_h; % change trace space
 nU = size(B,2); % num CG nodes
 nM = size(B,1); % num mortar nodes
 
@@ -76,8 +76,12 @@ Am = [RV B'
     B sparse(nM,nM)];
 bm = [b;zeros(nM,1)];
 um = Am\bm;
-
 U = um(1:nU);
+
+% S = B*(RV\B');
+% bS = B*(RV\b);
+% f = S\bS;
+% U = RV\(b - B'*f);
 
 I1 = 1:Np*K;
 I2 = Np*K + (1:Np*K);
@@ -88,7 +92,9 @@ tau2 = U(I2);
 v = U(I3);
 
 taunorm = sqrt(tau1.^2 + tau2.^2);
-plotSol(taunorm,25);title(['norm of tau, px = ', num2str(px)])
-plotSol(v,25);title(['v, px = ', num2str(px)])
-% figure;semilogy(svd(full(Adj_h)))
-keyboard
+plotSol(taunorm,25);%title(['norm of tau, px = ', num2str(px)])
+plotSol(v,25);%title(['v, px = ', num2str(px)])
+if (Np*K<1000)
+    figure;semilogy(svd(full(Adj_h)))
+end
+% keyboard
