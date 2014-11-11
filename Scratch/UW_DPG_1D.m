014d2c1830072f58f08e1483f26d136fc05872e9
Jesse Chan <jchan985@gmail.com>
2014-11-11 13:38:52 -0600

    uwvf with oas in 1D

--- Scratch/UW_DPG_1D.m
+++ Scratch/UW_DPG_1D.m
@@ -2,10 +2,11 @@
 % elements, with test order NT and trial order N
 
 % UW_DPG_1D(N,NT,K)
+clear
 
-N = 2;
+N = 4;
 NT = N+2; % test order
-K = 8;
+K = 2;
 
 r = JacobiGL(0,0,N);
 
@@ -24,15 +25,13 @@ VX = linspace(-1,1,K+1);
 x = ones(Np,1)*VX(1:K) + 0.5*(r+1)*(VX(2:K+1)-VX(1:K));
 xT = ones(NT+1,1)*VX(1:K) + 0.5*(rT+1)*(VX(2:K+1)-VX(1:K));
 h = diff(VX);h=h(1); J = h/2;
+nU = Np*K;
+nF = K+1;
 
 % define test operators
 IT = kron(speye(K),IT);
 DT = (1/J)*kron(speye(K),DT);
 MT = J*kron(speye(K),MT);
-RV = J*MT + (1/J)*DT'*MT*DT;
-
-% poisson = restricted
-B = -DT'*MT*IT;
 
 % jumps
 Em = sparse(K+1,(NT+1)*K);Ep = sparse(K+1,(NT+1)*K);
@@ -45,31 +44,56 @@ end
 Em(1,1) = 1;
 Ep(K+1,(NT+1)*K) = 1;
 Bhat = (Em-Ep)';
-% Bhat = blkdiag(Bhat,Bhat);
+
+% poisson = restricted
+% B = -DT'*MT*IT;
+RV = J*MT + (1/J)*DT'*MT*DT;
+
+B = [MT*IT -DT'*MT*IT;
+    -DT'*MT*IT 0*IT];
+RV = blkdiag(RV,RV);
+Bhat = blkdiag(Bhat,Bhat);
 
 % stiffness
 T = RV\[B Bhat];
 A = T'*[B Bhat];
 
 f = ones((NT+1)*K,1);
-b = [MT*f; zeros(K+1,1)];
-b = T'*MT*f;
+b = T'*[zeros((NT+1)*K,1);MT*f];
 
-%bcs
-nU = Np*K;
-nF = K+1;
-bc = nU+1;
+%bcs - left trace
+bc = 2*nU+1;
 A(bc,:) = 0; A(:,bc) = 0;
 A(bc,bc) = 1;
-% A(NpT,:) = 0;A(:,NpT) = 0; A(NpT,NpT) = 1;
-b(1) = 0;
-%b(NpT) = 0;
+b(bc) = 0;
 
-% fieldInds = 1:NpT;
-% fluxInds = NpT+1:size(A,1);
+% right trace
+bc = 2*nU + 1 + K;
+A(bc,:) = 0; A(:,bc) = 0;
+A(bc,bc) = 1;
+b(bc) = 0;
 
-u = A\b;
-figure
-plot(x(:),u(1:Np*K),'.-')
+if 0
+    u = A\b;
+    figure
+    plot(x(:),u(Np*K+1:2*Np*K),'.-')
+    xp = linspace(-1,1,100);
+    hold on;plot(xp,.5*(1+xp).*(1-xp),'r-')
+end
+
+P = sparse(size(A,1),size(A,2));
+for e = 1:K
+    ids{e} = [(1:Np) + Np*(e-1), (1:Np) + Np*(e-1) + Np*K, 2*Np*K + (e:e+1), 2*Np*K + K + 1 + (e:e+1)]; 
+    P(ids{e},ids{e}) = P(ids{e},ids{e}) + inv(A(ids{e},ids{e}));
+end
+cond(A)
+cond(P*A)
+
+
+
+
+
+
+
 
 
